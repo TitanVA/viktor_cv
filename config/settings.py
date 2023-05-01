26082c08b138d11660e8e95ecd7c7d3810a602a4
Viktor <viktorbezai@gmail.com>
2023-05-02 00:23:32 +0300

    Static

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    )
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
